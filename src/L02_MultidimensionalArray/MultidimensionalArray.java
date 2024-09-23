53a400d81765477f68e71e68ea6bfe0cb87522e1
DashadotT <markjovanoctat@gmail.com>
2024-09-23 19:56:49 +0800

    L02_3.3-Declaring Multidimensional Array

---
+++ src/L02_MultidimensionalArray/MultidimensionalArray.java
@@ -0,0 +1,16 @@
+package L02_MultidimensionalArray;
+
+public class MultidimensionalArray {
+
+    public static void main(String[] args) {
+
+        //Declaring multidimensional array
+        int[][] evenNumb = {
+            {2, 4, 6},
+            {8, 10, 12},
+            {14, 16, 18}
+        };
+
+    }
+
+}
