a5c6904384e48e56c29d54421de1dceeafb314cb
DashadotT <markjovanoctat@gmail.com>
2024-09-23 19:45:36 +0800

    L02_3.2-Declaring Array

---
+++ src/L02_DisplayViaForLoop/DisplayViaForLoop.java
@@ -0,0 +1,12 @@
+package L02_DisplayViaForLoop;
+
+public class DisplayViaForLoop {
+
+    public static void main(String[] args) {
+
+        //Declaring array
+        int[] OddNumbs = {1, 3, 5, 7, 9, 11, 13, 15};
+
+    }
+
+}
