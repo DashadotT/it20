6be76ecab0f12906d4567058e3b0879b6590621d
DashadotT <markjovanoctat@gmail.com>
2024-09-23 19:26:48 +0800

    L02_3.1-Declaring int arrays

---
+++ src/L02_SimpleImplementation/SimpleImplementation.java
@@ -0,0 +1,12 @@
+package L02_SimpleImplementation;
+
+public class SimpleImplementation {
+
+    public static void main(String[] args) {
+
+        //Simple Implementation
+        int[] OddNumbs = {1, 3, 5, 7};
+
+    }
+
+}
