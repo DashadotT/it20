95651eac8ac410cb489b13b752f2bf794dfd1371
DashadotT <markjovanoctat@gmail.com>
2024-09-25 16:13:55 +0800

    Added constructor and initializing variables

--- src/arraylist/ItemManager.java
+++ src/arraylist/ItemManager.java
@@ -1,5 +1,13 @@
 package arraylist;
 
+import java.util.ArrayList;
+
 public class ItemManager {
 
+    private ArrayList<String> items;
+
+    //Constructor to initialize the ArrayList
+    public ItemManager() {
+        items = new ArrayList<>();
+    }
 }
--- src/arraylist/Main.java
+++ src/arraylist/Main.java
@@ -1,9 +1,18 @@
 package arraylist;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        ItemManager list = new ItemManager();
+
+        //Initializing variables
+        String item1, item2, item3, newItem, finditem;
+        int index;
     }
 
 }
