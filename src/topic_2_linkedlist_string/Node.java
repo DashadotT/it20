b5ddebe5a11394494eaa36b980fdf619284eb6f8
DashadotT <markjovanoctat@gmail.com>
2024-10-02 19:18:04 +0800

    L05(String)-Added Linkedlist methods; add, printlist, delete, and move

--- src/topic_2_linkedlist_string/Node.java
+++ src/topic_2_linkedlist_string/Node.java
@@ -2,4 +2,13 @@ package topic_2_linkedlist_string;
 
 public class Node {
 
+    //Initializing variable;
+    String data;
+    Node next;
+
+    //Constructor
+    Node(String data) {
+        this.data = data;
+        this.next = null;
+    }
 }
