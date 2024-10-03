d51716eaccf3e6f8d13098eccdeb4d8b1a205c70
DashadotT <markjovanoctat@gmail.com>
2024-10-03 20:08:13 +0800

    L04(Int)-Added initializing variables and constructor in Node class

--- src/topic_2_linkedlist_int/Node.java
+++ src/topic_2_linkedlist_int/Node.java
@@ -2,4 +2,13 @@ package topic_2_linkedlist_int;
 
 public class Node {
 
+    //Initializing variable;
+    int data;
+    Node next;
+
+    //Constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
