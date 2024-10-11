b239ada672ace319627ebae174215ae1d149ba6d
DashadotT <markjovanoctat@gmail.com>
2024-10-11 17:06:41 +0800

    L06-Added scanner and initializing stack class with a capacity of 5

--- src/topic_3_stack/Main.java
+++ src/topic_3_stack/Main.java
@@ -1,8 +1,25 @@
 package topic_3_stack;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Stack st = new Stack(5);
+
+        while (true) {
+            System.out.println("================");
+            System.out.println("Choose number you want to perform"
+                    + "\n1. Stack size"
+                    + "\n2. Push number"
+                    + "\n3. Peek top Element"
+                    + "\n4. Pop top Element"
+                    + "\n5. Exit");
+            System.out.println("================");
+
+        }
 
     }
 
