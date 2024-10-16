de4c47c4251d5482418cbbff86313748970ebf3d
DashadotT <markjovanoctat@gmailc.com>
2024-10-16 10:32:52 +0800

    L07-Added objects for Queue and Scanner

--- src/topic_4_queue/Main.java
+++ src/topic_4_queue/Main.java
@@ -1,8 +1,13 @@
 package topic_4_queue;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Queue q = new Queue();
 
     }
 
