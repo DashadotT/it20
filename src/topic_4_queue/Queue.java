efa038346b85dbcfafe19b2c6dc4bd2a1cb97a7d
DashadotT <markjovanoctat@gmailc.com>
2024-10-16 10:16:39 +0800

    L07-Added Linkedlist and constructor to initialize the queue

--- src/topic_4_queue/Queue.java
+++ src/topic_4_queue/Queue.java
@@ -1,5 +1,13 @@
 package topic_4_queue;
 
+import java.util.LinkedList;
+
 public class Queue {
+
+    private LinkedList<Customer> customers;
+
+    public Queue() {
+        customers = new LinkedList<>();
+    }
 
 }
