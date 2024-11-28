6082f6c9083216ce2b3b6beaa228d2d8eef89922
DashadotT <markjovanoctat@gmailc.com>
2024-11-28 21:19:43 +0800

    Sorting-Added method for insertionSort with array parameter

--- src/topic_6_sorting/InsertionSort.java
+++ src/topic_6_sorting/InsertionSort.java
@@ -2,7 +2,22 @@ package topic_6_sorting;
 
 public class InsertionSort {
 
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
     public static void main(String[] args) {
+
     }
 
 }
