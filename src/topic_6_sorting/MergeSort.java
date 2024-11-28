08d7c5b9062bc55dd96b43f4214e017034b26009
DashadotT <markjovanoctat@gmailc.com>
2024-11-28 21:28:46 +0800

    Sorting-Added method for printing array

--- src/topic_6_sorting/MergeSort.java
+++ src/topic_6_sorting/MergeSort.java
@@ -36,6 +36,13 @@ public class MergeSort {
         merge(arr, left, right);
     }
 
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
     }
 
