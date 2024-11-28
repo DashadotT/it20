4f4c6bf09b0daaacf4d61da33f8aa9e5067e9cac
DashadotT <markjovanoctat@gmailc.com>
2024-11-28 21:20:27 +0800

    Sorting-Added method to print the array

--- src/topic_6_sorting/InsertionSort.java
+++ src/topic_6_sorting/InsertionSort.java
@@ -16,6 +16,13 @@ public class InsertionSort {
         }
     }
 
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
 
     }
