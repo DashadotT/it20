5a34d4fb886cc152e7fc8dc057fd5b994824ac85
DashadotT <markjovanoctat@gmailc.com>
2024-11-28 21:17:21 +0800

    Sorting-Added code in the main method that accept user inputs

--- src/topic_6_sorting/BubbleSort.java
+++ src/topic_6_sorting/BubbleSort.java
@@ -1,5 +1,7 @@
 package topic_6_sorting;
 
+import java.util.Scanner;
+
 public class BubbleSort {
 
     public static void bubbleSort(int[] arr) {
@@ -25,6 +27,26 @@ public class BubbleSort {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Original array:");
+        printArray(arr);
+        long startTime = System.currentTimeMillis();
+        bubbleSort(arr);
+        long endTime = System.currentTimeMillis();
+        long timeTaken = endTime - startTime;
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+        System.out.println("Sorting completed in: " + timeTaken + " milliseconds");
+        scanner.close();
     }
 
 }
