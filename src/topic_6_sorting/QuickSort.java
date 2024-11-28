eb04623e80340cc9de2368a505c54ad83da509cc
DashadotT <markjovanoctat@gmailc.com>
2024-11-28 21:38:14 +0800

    Sorting-Added method for quickSort

--- src/topic_6_sorting/QuickSort.java
+++ src/topic_6_sorting/QuickSort.java
@@ -22,6 +22,15 @@ public class QuickSort {
         return i + 1;
     }
 
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
     public static void main(String[] args) {
     }
 
