92161a20b3640dad2f5de73f7ea4d9ddc430d0ae
DashadotT <markjovanoctat@gmailc.com>
2024-11-28 21:12:36 +0800

    Sorting-Added method to print the array

--- src/topic_6_sorting/BubbleSort.java
+++ src/topic_6_sorting/BubbleSort.java
@@ -15,9 +15,16 @@ public class BubbleSort {
             }
         }
     }
-    
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        
+
     }
 
 }
